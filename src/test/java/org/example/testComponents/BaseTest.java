cb48d9f3a9360727456c81a0e87d2e7d26279116
Abthakur1 <absk.cts@gmail.com>
2024-09-22 13:40:29 +0530

    refactoring code

--- src/test/java/org/example/testComponents/BaseTest.java
+++ src/test/java/org/example/testComponents/BaseTest.java
@@ -44,7 +44,7 @@ public class BaseTest {
             driver = new ChromeDriver(options);
         }
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
+//        driver.manage().window().maximize();
         return driver;
 
     }
