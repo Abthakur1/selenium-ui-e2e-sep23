333785d85941041bdab82dee0af8d6ba56fb688d
Abthakur1 <absk.cts@gmail.com>
2024-09-22 13:22:34 +0530

    test for ci

---
+++ src/test/java/org/example/testCases/SampleTestCaseForCI.java
@@ -0,0 +1,10 @@
+package org.example.testCases;
+
+import org.testng.annotations.Test;
+
+public class SampleTestCaseForCI {
+    @Test
+    public void CiTest(){
+        System.out.println("CI Test executed");
+    }
+}
